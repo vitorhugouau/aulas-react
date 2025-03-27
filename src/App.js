76436d359548517f6731f77d5beb9e6264e38635
Vitor <vitorhugkrodrigues@gmail.com>
2025-03-27 19:41:17 -0300

    feat: alter color title

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ const App = () => {
     return (
         <div className="body">
             <div className="top-bar">
-                <h1>Personagens de Rick and Morty       
+                <h1 style={{ color: '#97CE4C', textShadow: '2px 2px 4px #2C0735' }}>
+                    Personagens de Rick and Morty
                 </h1>
+
             </div>
             <ApiCard />
         </div>
